72c2332bf0840955dd20b57fd6a09ac02f09fc1d
mukxabek2010-droid <mukxabek2010@gmail.com>
2026-07-14 11:22:23 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,8 +28,8 @@ MONGO_URI         = os.getenv("MONGO_URI")
 
 REQUIRED_CHANNELS = ["@roblox_chat_veko", "@uzbekroblox", "@trade_chanel_uz"]
 TRADE_CHANNEL     = "@trade_chanel_uz"
-CARD_NUMBER       = os.getenv("CARD_NUMBER", "5614682091344749")
-CARD_OWNER        = os.getenv("CARD_OWNER", "Nurboyev.N")
+CARD_NUMBER       = os.getenv("CARD_NUMBER", "9860080394103636")
+CARD_OWNER        = os.getenv("CARD_OWNER", "Mashrapova.D")
 CHAT_LINK         = os.getenv("CHAT_LINK", "https://t.me/roblox_chat_veko")
 ROBLOX_SCRIPT_CHANNEL = os.getenv("ROBLOX_SCRIPT_CHANNEL", "https://t.me/deltauzbrb")
 
@@ -43,7 +43,7 @@ ADMIN_ID = 8667862086
 # ═══════════════════════════════════════════════════════
 # ADMIN ROLLARI TIZIMI (Super / Referal / Robux / Mashkalar)
 # ═══════════════════════════════════════════════════════
-ROBUX_ADMIN_CONTACT = "@robuxmarketadmin"
+ROBUX_ADMIN_CONTACT = "@its_vekoo"
 
 ADMIN_ROLE_LABELS = {
     "super":    "👑 Super admin",
